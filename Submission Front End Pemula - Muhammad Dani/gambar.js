0f70f1a7f208a09b3d2a9ca88e1c7cf816f37fa8
Muhammad Dani <mhmmdani96@gmail.com>
2022-06-19 22:51:40 +0700

    Submission Dicoding Front End
    Html

---
+++ Submission Front End Pemula - Muhammad Dani/gambar.js
@@ -0,0 +1,7 @@
+//Jalankan kode berikut pada console browser kemudian gambar pada halaman akan berubah.
+let kroyaImage = document.querySelector("#kroyaImage");
+kroyaImage.setAttribute("src","assets/image/kampung-kuliner.jpg");
+
+/*mengambil nilai dari atribut yang diterapkan
+kemudian jalankan pada console*/
+kroyaImage.getAttribute("src");
